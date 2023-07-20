c65bd83ee82229d997380c17cb18f1032f5bed84
Rushi <@ns.rushi@gmail.com>
2023-07-20 11:57:47 -0700

    udpated

--- main.tf
+++ main.tf
@@ -47,4 +47,77 @@ resource "aws_internet_gateway" "gw" {
     Name = "igw"
   }
 }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block  = "0.0.0.0/0"
+    gateway_id  = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_instance" "web_server" {
+  ami                    = "ami-0f8ca728008ff5af4"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+
+ user_data = <<-EOF
+    #!/bin/bash
+
+    # Update the package list
+    sudo apt update
+
+    # Install Apache
+    sudo apt install -y apache2
+    sudo cat <<HTML > /var/www/html/index.html
+    <!DOCTYPE html>
+    <html><body><h1>Welcome to my website</h1></body></html>
+    HTML
+
+    sudo systemctl restart apache2
+  EOF
+
+ tags = {
+    Name = "terraform-instance"
+  }
+}
+ resource "aws_security_group" "ssh_access" {
+   name_prefix = "web-server-sg"
+   vpc_id = aws_vpc.main.id
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+ }
+ egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = -1
+     cidr_blocks = ["0.0.0.0/0"]
+ }
+ }
+ resource "aws_eip" "ip" {
+   instance = aws_instance.server_terraform.id
+   vpc      = true
+   tags = {
+     Name = "elastic-ip"
+   }
+ }
